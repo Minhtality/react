91a7093e07908d733c2896c2151d6ddd08f1ab04
Minhtality <37172196+Minhtality@users.noreply.github.com>
2019-08-27 21:30:04 -0700

    finished todo front-end, need to add DB

---
+++ client/src/components/TodoItem.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class TodoItem extends Component {
+
+  getStyle = () => {
+    return {
+      background: 'rgba(0,0,0,.4)',
+      padding: '1rem',
+      color: '#fff',
+      borderBottom: '1px #ccc dotted',
+      textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+    }
+  }
+
+  render() {
+    //destructuring
+    const { id, title } = this.props.todo;
+    return (
+      <div style={this.getStyle()}>
+        <p>
+          <input type="checkbox" onChange={this.props.toggleComplete.bind(this, id)} />{' '}
+          {title}
+          <button style={btnStyle} onClick={this.props.delTodo.bind(this, id)}>x</button>
+        </p>
+      </div>
+    );
+  }
+}
+//Button Styles
+const btnStyle = {
+  background: '#FF6666',
+  color: '#fff',
+  border: 'none',
+  padding: '5px 9px',
+  borderRadius: '50%',
+  cursor: 'pointer',
+  float: 'right'
+}
+// PropType
+TodoItem.propTypes = {     // This should be the name of the main class file
+  todo: PropTypes.object.isRequired,
+  toggleComplete: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired,
+}
+
+export default TodoItem;
